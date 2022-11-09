21314d2939c9b480da04a0d8651604b3f90f2627
merpich <merpich.dev@gmail.com>
2022-11-10 00:13:14 +0300

    Создана страница NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
+import { NavButton } from '../components/NavButton/NavButton';
+import styles from '../styles/modules/NotFound.module.scss';
+
 export function NotFound() {
 	return (
-		<h1>Not Found</h1>
+		<div className={styles.wrapper}>
+			<div className={styles.container}>
+				<div className={styles.message}>
+					<h1 className={styles.title}>Ничего не найдено</h1>
+					<h2 className={styles.subtitle}>Похоже, данной страницы не существует...</h2>
+				</div>
+				<NavButton url="/" text="Вернуться на сайт" />
+			</div>
+		</div>
 	);
 }
