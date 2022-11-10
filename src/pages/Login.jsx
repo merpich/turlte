5e3249a3ba69fb7d67e78b1af1d2580a8029d9f0
merpich <merpich.dev@gmail.com>
2022-11-10 18:46:32 +0300

    Создана страница входа

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,56 @@
+import { useState } from 'react';
+import styles from '../styles/modules/Login.module.scss';
+
 export function Login() {
+	const [activeAuth, setActiveAuth] = useState(true);
+	const [activeReg, setActiveReg] = useState(false);
+
+	function clickSwitcher(event) {
+		const textContent = event.target.textContent;
+
+		if (textContent === 'Авторизация') {
+			if (!activeAuth) {
+				setActiveAuth(true);
+				setActiveReg(false);
+			}
+		} else if (!activeReg) {
+			setActiveAuth(false);
+			setActiveReg(true);
+		}
+	}
+
+	function login(event) {
+		event.preventDefault();
+	}
+
 	return (
-		<h1>Login</h1>
+		<div className={styles.wrapper}>
+			<div className={styles.controls}>
+				<button
+					className={`${styles.switcher} ${activeAuth ? styles.switcher_active : ''}`}
+					onClick={clickSwitcher}
+				>
+					Авторизация
+				</button>
+				<button
+					className={`${styles.switcher} ${activeReg ? styles.switcher_active : ''}`}
+					onClick={clickSwitcher}
+				>
+					Регистрация
+				</button>
+			</div>
+
+			<form className={styles.form} method="post" onSubmit={login}>
+				{activeAuth
+					? (
+						<p>Авторизация</p>
+					) : (
+						<p>Регистрация</p>
+					)}
+				<button className={styles.button} type="submit">
+					Войти
+				</button>
+			</form>
+		</div>
 	);
 }
